8b3c270db31c34181a81352046636de2fb71c4d4
Caleb Wiley <180115515+Caleb-Wiley@users.noreply.github.com>
2025-05-06 23:02:18 -0500

    PID v0.1
    functions with heading

--- FSW/PID Board.ino
+++ FSW/PID Board.ino
@@ -13,6 +13,116 @@
 #include "FeedBackServo.h"
 #include <Servo.h>
 
+
+
+
+const int altitude = 0;
+
+const int gyroX = 0;
+const int gyroY = 0;
+const int gyroZ = 0;
+const int accelX = 0;
+const int accelY = 0;
+const int accelZ = 0;
+
+
+
+
+
+//----------------Tilt compensation----------------
+#include <Wire.h>
+#include <LIS3MDL.h>
+#include <LSM6.h>
+
+LIS3MDL mag;
+LSM6 imu;
+
+/*
+Calibration values; the default values of +/-32767 for each axis
+lead to an assumed magnetometer bias of 0. Use the Calibrate example
+program to determine appropriate values for your particular unit.
+*/
+LIS3MDL::vector<int16_t> m_min = { -32767, -32767, -32767 };
+LIS3MDL::vector<int16_t> m_max = { +32767, +32767, +32767 };
+
+
+/*
+Returns the angular difference in the horizontal plane between the
+"from" vector and north, in degrees.
+
+Description of heading algorithm:
+Shift and scale the magnetic reading based on calibration data to find
+the North vector. Use the acceleration readings to determine the Up
+vector (gravity is measured as an upward acceleration). The cross
+product of North and Up vectors is East. The vectors East and North
+form a basis for the horizontal plane. The From vector is projected
+into the horizontal plane and the angle between the projected vector
+and horizontal north is returned.
+*/
+template<typename T> float computeHeading(LIS3MDL::vector<T> from) {
+  LIS3MDL::vector<int32_t> temp_m = { mag.m.x, mag.m.y, mag.m.z };
+
+  // copy acceleration readings from LSM6::vector into an LIS3MDL::vector
+  LIS3MDL::vector<int16_t> a = { imu.a.x, imu.a.y, imu.a.z };
+
+  // subtract offset (average of min and max) from magnetometer readings
+  temp_m.x -= ((int32_t)m_min.x + m_max.x) / 2;
+  temp_m.y -= ((int32_t)m_min.y + m_max.y) / 2;
+  temp_m.z -= ((int32_t)m_min.z + m_max.z) / 2;
+
+  // compute E and N
+  LIS3MDL::vector<float> E;
+  LIS3MDL::vector<float> N;
+  LIS3MDL::vector_cross(&temp_m, &a, &E);
+  LIS3MDL::vector_normalize(&E);
+  LIS3MDL::vector_cross(&a, &E, &N);
+  LIS3MDL::vector_normalize(&N);
+
+  // compute heading
+  float heading = atan2(LIS3MDL::vector_dot(&E, &from), LIS3MDL::vector_dot(&N, &from)) * 180 / PI;
+  if (heading < 0) heading += 360;
+  return heading;
+}
+
+/*
+Returns the angular difference in the horizontal plane between a
+default vector (the +X axis) and north, in degrees.
+*/
+float computeHeading() {
+  return computeHeading((LIS3MDL::vector<int>){ 1, 0, 0 });
+}
+
+
+
+
+
+
+
+
+
+//debugging:
+const bool DEBUG = true;
+
+void debugCheckpoint(const char *message) {
+  static int checkpoint = 0;  // Static variable retains its value between calls
+  if (DEBUG) {
+    checkpoint++;  // Increment the checkpoint number
+    Serial.print("Checkpoint ");
+    Serial.print(checkpoint);  // Print the current checkpoint number
+    Serial.print(": ");
+    Serial.println(message);  // Print the provided message
+  }
+}
+
+
+
+
+
+
+
+
+
+
 // PINS AND DEFINITIONS
 #define SD_CS_PIN 6  // Chip select pin for SD card
 
@@ -243,8 +353,8 @@ void SingleShotMeasure_setup() {
   // Choose the desired configuration of the sensor. In this example we will use the Lowest Noise settings from the datasheet
   ens220.setDefaultConfiguration();
   ens220.setPressureConversionTime(ENS220::PressureConversionTime::T_16_4);
-  ens220.setOversamplingOfPressure(ENS220::Oversampling::N_128);
-  ens220.setOversamplingOfTemperature(ENS220::Oversampling::N_128);
+  ens220.setOversamplingOfPressure(ENS220::Oversampling::N_8);
+  ens220.setOversamplingOfTemperature(ENS220::Oversampling::N_8);
   ens220.setPressureTemperatureRatio(ENS220::PressureTemperatureRatio::PT_1);
   ens220.setStandbyTime(ENS220::StandbyTime::OneShotOperation);
   ens220.setPressureDataPath(ENS220::PressureDataPath::Direct);
@@ -277,21 +387,41 @@ void SingleShotMeasure_loop() {
 }
 
 void setup() {
-  Serial.begin(2000000);  // Debugging output
+  //----------------Tilt compensation----------------
+  Serial.begin(115200);
+  delay(5000);
+  Serial.println("hi");
   Wire.begin();
   Wire.setClock(400000);
+  debugCheckpoint("wire set");
+  if (!mag.init()) {
+    Serial.println("Failed to detect and initialize LIS3MDL magnetometer!");
+  }
+  mag.enableDefault();
+
+  if (!imu.init()) {
+    Serial.println("Failed to detect and initialize LSM6 IMU!");
+  }
+  imu.enableDefault();
+  //----------------end tilt compensation settup---------
+  debugCheckpoint("sensor init 1");
+
+  /*--removed for non-functionality
   // Initialize camera servo
   camServo.attach(SERVO_PIN);
+  */
 
+  debugCheckpoint("servo");
 
   // Initialize cameras control pins (e.g., for power on/off)
   pinMode(PULSECOMS, INPUT);
   attachInterrupt(digitalPinToInterrupt(PULSECOMS), readPulseComs, RISING);  // Trigger on HIGH pulse
+  debugCheckpoint("interupt pin enable");
 
   pinMode(CAMERA2_PIN, OUTPUT);
   digitalWrite(CAMERA2_PIN, LOW);
 
-
+  debugCheckpoint("camera stuff");
 
   // Initialize SD card
   if (!SD.begin(SD_CS_PIN)) {
@@ -300,6 +430,8 @@ void setup() {
     Serial.println("SD card initialized successfully");
   }
   backupFile = SD.open("backup.txt", FILE_WRITE);
+
+  debugCheckpoint("sd");
 
   if (!lis3mdl_FC.begin_I2C(MAG1_I2C_ADDRESS)) {
     Serial.println("Failed to find LIS3MDL #1");
@@ -332,11 +464,37 @@ void setup() {
     lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
     lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
   }
+  debugCheckpoint("round two sensor init");
 
   SingleShotMeasure_setup();
 }
 
 void loop() {
+
+
+
+  //----------------Tilt compensation----------------
+  mag.read();
+  imu.read();
+
+  /*
+  When given no arguments, the heading() function returns the angular
+  difference in the horizontal plane between a default vector (the
+  +X axis) and north, in degrees.
+  */
+  float heading = computeHeading((LIS3MDL::vector<int>){ 1, 0, 0 });
+
+  /*
+  To use a different vector as a reference, use the version of
+  computeHeading() that takes a vector argument; for example, call it like this
+  to use the -Z axis as a reference:
+  */
+
+
+  Serial.println(heading);
+  delay(100);
+  //----------------Tilt compensation end----------------
+
 
   unsigned long missionTime = millis() / 1000;  // Mission time in seconds
 
@@ -363,22 +521,22 @@ void loop() {
   if (IMU.gyroscopeAvailable()) {
     IMU.readGyroscope(gyroX, gyroY, gyroZ);
   }
-
+  debugCheckpoint("pre-singleshot");
   // Retrieve Temperature and Pressure from ENS220 (Single Shot Mode)
   SingleShotMeasure_loop();
+  debugCheckpoint("successfull singleshot");
 
   // Calculate altitude from pressure
   float altitude = calculateAltitude(pressure);
   char telemetry[256];  // Adjust the size if necessary
+  debugCheckpoint("pre-telemetry");
   snprintf(telemetry, sizeof(telemetry),
-           "%s,%s,%u,%s,%s,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%u,%s,%.1f,%.4f,%.4f,%u,%s,COSMOS",
-           TEAM_ID, "00:00:00", packetCount, "mode", "state",
+           "%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,COSMOS",
            altitude, temperature, pressure, "currentVoltage",
            gyroX, gyroY, gyroZ, accelX, accelY, accelZ,
            magEvent1.magnetic.x, magEvent1.magnetic.y,
-           magEvent1.magnetic.z, "rpm", "gpsTime", "gpsAltitude",
-           "latitude", "longitude", "satellites", "lastCommand");
-
+           magEvent1.magnetic.z);
+  debugCheckpoint("successfull telemetry");
   Serial.println(telemetry);
   // Save telemetry to SD card
   dataFile = SD.open("data.txt", FILE_WRITE);
@@ -391,10 +549,15 @@ void loop() {
   }
   packetCount++;  // Increment packet count
 
+
+  /*-------dated heading calc---------
   // Calculate heading
   heading = atan2(magEvent2.magnetic.y, magEvent2.magnetic.x);
   heading = heading * 180 / PI;     // Convert to degrees
   if (heading < 0) heading += 360;  // Ensure 0-360 range
+  */
+
+
 
   int comsState = digitalRead(FC_COMS);
 
